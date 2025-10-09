9af732d83279b901bfb88ce4e631d8cdfd393f74
deven367 <masterdeven@gmail.com>
2025-10-08 23:50:20 -0400

    update docstrings

--- ttf/talk.py
+++ ttf/talk.py
@@ -1,33 +1,36 @@
 import click
+import pandas as pd
 from PyPDF2 import PdfReader
 
 from ttf.utils import chat_with
 
 
-def chat_with_file(path: str, prompt:str) -> str:
-    """_summary_
+def chat_with_file(path: str, prompt: str) -> str:
+    """
+    Chat with a file.
 
     Args:
-        path (str): _description_
-        prompt (str): _description_
+        path (str): The path to the file.
+        prompt (str): The prompt to the chatbot.
 
     Returns:
-        str: _description_
+        str: The response from the chatbot.
     """
 
-    if path.endswith('.pdf'):
+    if path.endswith(".pdf"):
         reader = PdfReader(path)
         text = "\n".join([page.extract_text() for page in reader.pages])
 
-    elif path.endswith('.txt'):
-        text = open(path, 'r').read()
+    elif path.endswith(".txt"):
+        text = open(path, "r").read()
+    elif path.endswith(".csv"):
+        text = pd.read_csv(path)[["date", "text"]][500:600].to_string()
     else:
-        extension = path.split('.')[-1]
+        extension = path.split(".")[-1]
         raise ValueError(f'Files ending in "{extension}" are not yet supported')
 
     user_input = prompt + ":\n\n" + text
     chat_with(user_input)
-
 
 
 @click.command()
@@ -35,6 +38,7 @@ def chat_with_file(path: str, prompt:str) -> str:
 @click.option("--prompt", "-pr", type=str, default="Summarize the following text")
 def main(file, prompt):
     chat_with_file(file, prompt)
+
 
 if __name__ == "__main__":
     main()
